078a470291dd3dd35e0855cef9f271785d0f1a3b
Pronothurah <onsongojosphat28@gmail.com>
2024-03-02 20:32:28 +0300

    Introducing the templates folder

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -34,7 +34,7 @@ def hbnb():
     # Generate a UUID
     cache_id = uuid.uuid4()
 
-    return render_template('100-hbnb.html',
+    return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
